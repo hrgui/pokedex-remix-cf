c42deec45044e900e3bfdff0a8a22d059ca3df37
Harman Goei <5001432+hrgui@users.noreply.github.com>
2021-12-19 18:29:20 -0800

    fix(Grid): Grid not showing properly in mobile + begin filters

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -35,28 +35,48 @@ export function Pagination({ currentPage, maxPages }: { currentPage: number; max
   );
 }
 
+export function PokemonGrid({ results }: { results: any[] }) {
+  return (
+    <div className="grid grid-cols-4 gap-4 sm:ml-4 mt-4">
+      {results.map((result) => (
+        <Link className="col-span-4 m-2 sm:col-span-1" to={`/pokemon/${result.id}`}>
+          <div key={result.id} className="rounded relative">
+            <div
+              className="p-4 absolute w-full h-full"
+              style={{ backgroundColor: `${result.color}`, opacity: 0.1 }}
+            ></div>
+            <div className="p-4">
+              <img src={result.image} alt={result.name} loading="lazy" className="min-w-full" />
+            </div>
+            <div className="text-white grid grid-cols-3 bg-black overflow-hidden rounded-b-lg">
+              <div className="bg-red-500 col-span-1 p-2 pl-6 -skew-x-12 -ml-2">#{result.id}</div>
+              <div className=" col-span-2 p-2 pl-4">{startCase(result.name)}</div>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export function PokemonFilters({ options, value }: { options: any; value: any }) {
+  return (
+    <div className="w-full sm:w-64 flex-none mt-6 p-4 rounded bg-red-200">
+      <header className="flex justify-between">
+        <div className="text-bold">Filters</div>
+        <div>Reset</div>
+      </header>
+    </div>
+  );
+}
+
 export default function () {
-  const { results, maxPages, currentPage } = useLoaderData<any>();
+  const { results, filterOptions, filterValues, maxPages, currentPage } = useLoaderData<any>();
   return (
     <div>
-      <div className="grid grid-cols-4 gap-4 mt-4">
-        {results.map((result) => (
-          <Link to={`/pokemon/${result.id}`}>
-            <div key={result.id} className="rounded relative">
-              <div
-                className="p-4 absolute w-full h-full"
-                style={{ backgroundColor: `${result.color}`, opacity: 0.1 }}
-              ></div>
-              <div className="p-4">
-                <img src={result.image} alt={result.name} loading="lazy" className="min-w-full" />
-              </div>
-              <div className="text-white grid grid-cols-3 bg-black overflow-hidden rounded-b-lg">
-                <div className="bg-red-500 col-span-1 p-2 pl-6 -skew-x-12 -ml-2">#{result.id}</div>
-                <div className=" col-span-2 p-2 pl-4">{startCase(result.name)}</div>
-              </div>
-            </div>
-          </Link>
-        ))}
+      <div className="sm:flex">
+        <PokemonFilters options={filterOptions} value={filterValues} />
+        <PokemonGrid results={results} />
       </div>
       <Pagination currentPage={currentPage} maxPages={maxPages} />
     </div>
